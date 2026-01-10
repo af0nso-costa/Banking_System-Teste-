4da99fca640c4266de485dcc1afc31666aee0ecd
Afonso Costa <afonso007costa@gmail.com>
2026-01-10 07:47:57 +0000

    Delete src directory

--- src/models/CanalAcesso.java
+++
@@ -1,9 +0,0 @@
-package src.models;
-
-public abstract class CanalAcesso {
-
-  public abstract void consultarSaldo(Conta conta, Cliente cliente);
-
-  public abstract void verMovimentos(Conta conta, Cliente cliente);
-
-}    
